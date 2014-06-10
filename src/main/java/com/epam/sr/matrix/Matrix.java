2a3590b57d041b29a82f1ffc9aabe94016907b2d
Rodion <rody10@mail.ru>
2014-06-11 00:25:19 +0600

    Print was change

--- src/main/java/com/epam/sr/matrix/Matrix.java
+++ src/main/java/com/epam/sr/matrix/Matrix.java
@@ -1,5 +1,6 @@
 package com.epam.sr.matrix;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Matrix {
@@ -8,6 +9,7 @@ public class Matrix {
     private int rowsCount;
     private int colsCount;
     private int[][] values;
+    int[][] multipledMatrix;
 
     public Matrix(int rowsCount, int colsCount) {
         this.rowsCount = rowsCount;
@@ -40,7 +42,7 @@ public class Matrix {
 
     public void multiplyMatrix(int[][] matrix2) {
 
-        int[][] multiMatrix = new int[rowsCount][matrix2[0].length]; // A new array for multipled values
+        multipledMatrix = new int[rowsCount][matrix2[0].length]; // A new array for multipled values
 
         for (int i = 0; i < rowsCount; i++) { //going to the rows of the first values
             for (int l = 0; l < matrix2[0].length; l++) { //going to the columns of the second values
@@ -48,20 +50,29 @@ public class Matrix {
                 for (int j = 0; j < colsCount; j++) {      //going to the columns of the fist values and the rows of the second
                     sum = sum + values[i][j] * matrix2[j][l];
                 }
-                multiMatrix[i][l] = sum; //wright new element for multipled values
+                multipledMatrix[i][l] = sum; //wright new element for multipled values
             }
         }
 
         System.out.println("Multipled Matrix");
 
-        for (int i = 0; i < multiMatrix.length; i++) {
+        System.out.println(toString());
+
+        for (int i = 0; i < multipledMatrix.length; i++) {
             String s = "";
-            for (int j = 0; j < multiMatrix[0].length; j++) {
-                s = s + multiMatrix[i][j] + " ";
+            for (int j = 0; j < multipledMatrix[0].length; j++) {
+                s = s + multipledMatrix[i][j] + " ";
             }
 
             System.out.println(s);
         }
+    }
+
+    @Override
+    public String toString() {
+        return "Matrix{" +
+                "values=" + Arrays.deepToString(multipledMatrix) +
+                '}';
     }
 
     public int[][] getValues() {
